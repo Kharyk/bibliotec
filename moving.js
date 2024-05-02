eee474c001d442b21a1dd5ab5ddfcd697443651d
Kharyk <131535513+Kharyk@users.noreply.github.com>
2024-05-02 19:43:09 +0200

    Create moving.js

---
+++ moving.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollLeftButton = document.querySelector(".scroll-left");
+  const scrollRightButton = document.querySelector(".scroll-right");
+  const bookShelf = document.querySelector(".book-shelf");
+
+  scrollLeftButton.addEventListener("click", function () {
+      bookShelf.scrollBy({
+          left: -200,
+          behavior: "smooth"
+      });
+  });
+
+  scrollRightButton.addEventListener("click", function () {
+      bookShelf.scrollBy({
+          left: 200,
+          behavior: "smooth"
+      });
+  });
+});
+
+
+
+
+function searchBooks() {
+  var searchInput = document.getElementById("searchInput").value.toLowerCase();
+  var books = document.querySelectorAll(".book img");
+
+  books.forEach(function(book) {
+      var altText = book.alt.toLowerCase();
+      if (altText.includes(searchInput)) {
+          book.closest('.book').style.display = "inline-block";
+      } else {
+          book.closest('.book').style.display = "none";
+      }
+  });
+}
+
+
+function scrollToBottom() {
+  window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth'
+  });
+}
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 500) {
+      document.querySelector('.scroll-down-button').style.display = 'block';
+  } else {
+      document.querySelector('.scroll-down-button').style.display = 'none';
+  }
+});
